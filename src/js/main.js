457593ff6dab1ff53b42c42b74b75089e7dad2f6
DanMan3 <dhbennion@gmail.com>
2026-01-14 14:22:56 -0700

    Dynamic Product List activity completed. Changed the lint quotes back to double

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+import ProductList from "./ProductList.mjs";
+import ProductData from "./ProductData.mjs";
+
+const dataSource = new ProductData("tents");
+
+const listElement = document.querySelector(".product-list");
+
+const productList = new ProductList({
+  category: "tents",
+  dataSource,
+  listElement,
+});
+
+productList.init();
