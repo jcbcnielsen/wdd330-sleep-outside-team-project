eeec125095b12eb2aaf849dce57d2ed84a3abe3a
DanMan3 <dhbennion@gmail.com>
2026-01-16 09:37:13 -0700

    Total$ in Cart bug fix applied

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,14 @@
-import { getParam } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
 
-const details = new ProductDetails(productId, dataSource);
-details.init();
-
+function addProductToCart(product) {
+  const existing = getLocalStorage("so-cart") || [];
+  const cart = Array.isArray(existing) ? existing : existing ? [existing] : [];
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
+}
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -15,7 +16,4 @@ async function addToCartHandler(e) {
 }
 
 // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
+document.getElementById("addToCart").addEventListener("click", addToCartHandler);
